ef5d0afef0e5234417f8c0cb3835fd45644fe8f9
Nzer-Suleiman <87446598+Nzer-Suleiman@users.noreply.github.com>
2023-02-17 19:31:53 +0200

    some update

--- Week 024 - Date, Generators, Modules/js/mod-two.js
+++ Week 024 - Date, Generators, Modules/js/mod-two.js
@@ -1,5 +1,5 @@
+export {a as numOne, b as numTwo, c as numThree};
+
 let a = 10; // Do Not Edit Names
 let b = 20; // Do Not Edit Names
 let c = 30; // Do Not Edit Names
-
-export {a as numOne, b as numTwo, c as numThree};
